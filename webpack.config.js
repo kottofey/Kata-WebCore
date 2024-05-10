cc7097bb5a3abd5a5639754ddf94d7bab1c0ff59
kottofey (KotBookPro) <romeokontakt@gmail.com>
2024-05-11 02:28:17 +0300

    Making GH Pages to upload chages
    Changed webpack output folder from dist to docs as GH Pages wants to build site from...

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 	},
 	output: {
 		filename: '[name].[contenthash].js',
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'docs'),
 		clean: true,
 	},
 	plugins: [
